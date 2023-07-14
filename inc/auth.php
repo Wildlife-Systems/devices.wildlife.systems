592a42388a8f8619d064f1d0a788814a31358bc2
Ed Baker <edwbaker@gmail.com>
2023-07-14 11:56:11 +0000

    auth_node()

---
+++ inc/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+# Verify node_id against token using
+function auth_node($node_id, $token, $db) {
+  $sql = "SELECT * FROM `nodes` WHERE `node_id` = '$node_id' AND `token` = '$token'";
+  $result = $db->query($sql);
+  if ($result->num_rows = 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
