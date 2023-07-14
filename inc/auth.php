70a5560db9763fcff521664d8f87294e3fb5db98
Ed Baker <edwbaker@gmail.com>
2023-07-14 14:56:30 +0000

    Fix row count check.

--- inc/auth.php
+++ inc/auth.php
@@ -2,9 +2,9 @@
 
 # Verify node_id against token using
 function auth_node($node_id, $token, $db) {
-  $sql = "SELECT * FROM `nodes` WHERE `node_id` = '$node_id' AND `token` = '$token'";
+  $sql = "SELECT * FROM `nodes` WHERE `node_id` = '$node_id' AND `node_token` = '$token'";
   $result = $db->query($sql);
-  if ($result->num_rows = 1) {
+  if ($result->num_rows == 1) {
     return true;
   } else {
     return false;
