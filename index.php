8f4a1c1b7a5d84ff14a2a4c9081945cc4b658f02
Ed Baker <edwbaker@gmail.com>
2023-07-18 08:43:51 +0000

    Add node_id to devices home

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <html>
 <head>
-<title>devices.wildlife.systems</title>
+  <title>devices.wildlife.systems</title>
+  <link rel="icon" type="image/png" href="https://devices.wildlife.systems/favicon.png" />
+  <link rel="stylesheet" href="https://audioblast.org/ab-api.css">
 </head>
 
 <body>
-<h1>devices.wildlife.systems</h1>
+<div id="title">
+  <a href="/">
+    <img src="https://cdn.audioblast.org/wildlifesystems_flash.png"
+    alt="WildlifeSytems flash logo"
+    class="audioblast-flash" /></a>
+  <h1>devices.wildlife.systems</h1>
+</div>
 
 <p>Devices currently reporting their status to the wildlife.systems network.</p>
 
 <?php
 include ("config/db.php");
 
-$sql = "SELECT `hostname`, `status`, UNIX_TIMESTAMP()-`timestamp` AS `ago` FROM ws.heartbeats;";
+$sql = "SELECT `node_id`, `hostname`, `status`, UNIX_TIMESTAMP()-`timestamp` AS `ago` FROM ws.heartbeats;";
 $result = $db->query($sql);
 if ($result->num_rows > 0) {
-  echo "<table><tr><th>Hostname</th><th>Current action</th><th>Seconds since last update</th></tr>";
+  echo "<table><tr><th>Node ID</th><th>Hostname</th><th>Current action</th><th>Seconds since last update</th></tr>";
   while($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["hostname"] . "</td><td>" . $row["status"] . "</td><td>" . $row["ago"] . "</td></tr>";
+    echo "<tr><td>" . $row["node_id"] . "</td><td>" . $row["hostname"] . "</td><td>" . $row["status"] . "</td><td>" . $row["ago"] . "</td></tr>";
   }
   echo "</table>";
 } else {
-  echo "0 results";
+  echo "No devices reporting.";
 }
 $db->close();
 
